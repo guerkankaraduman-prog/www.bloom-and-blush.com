6262480da753b7d76ed2db436869c16786ba50b3
guerkankaraduman-prog <guerkan.karaduman@gmail.com>
2026-05-03 17:10:28 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-console.log("Site loaded");
+fetch("data/posts.json")
+  .then(res => res.json())
+  .then(data => {
+    const container = document.getElementById("posts-container");
+
+    if(container){
+      data.forEach(post => {
+        container.innerHTML += `
+          <div class="post">
+            <h3>${post.title}</h3>
+            <p>${post.excerpt}</p>
+            <a href="post.html?id=${post.id}">Read More</a>
+          </div>
+        `;
+      });
+    }
+  });
